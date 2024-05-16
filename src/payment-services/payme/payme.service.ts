ff34f19560d0300c22788a301714b570fb4651cd
bek <shoyatbekov03032003@gmail.com>
2024-05-16 16:43:44 +0500

    payme: 1.0.0 test

--- src/payment-services/payme/payme.service.ts
+++ src/payment-services/payme/payme.service.ts
@@ -109,6 +109,7 @@ export class PaymeService {
           message: 'Transaction already created',
           data: null,
         };
+
       const currentTime = Date.now();
 
       const expirationTime =
