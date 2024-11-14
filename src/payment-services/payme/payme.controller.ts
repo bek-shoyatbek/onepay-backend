b2d60eec027302416aa6a619fd00cf082fd6d636
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-14 18:04:25 +0500

    feat: payme responsing way changed

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -4,23 +4,26 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { PaymeService } from './payme.service';
 import { RequestBody } from './types/incoming-request-body';
 import { PaymeBasicAuthGuard } from 'src/auth/guards/payme.guard';
-import { log } from 'console';
+import { Response } from 'express';
 
 @Controller('payme')
 export class PaymeController {
-  constructor(private readonly paymeService: PaymeService) {}
+  constructor(private readonly paymeService: PaymeService) { }
+
+
   @Post()
   @UseGuards(PaymeBasicAuthGuard)
   @HttpCode(HttpStatus.OK)
-  async handleTransactionMethods(@Body() reqBody: RequestBody) {
-    log('payme:reqBody', reqBody);
+  async handleTransactionMethods(@Body() reqBody: RequestBody, @Res() res: Response) {
     const result = await this.paymeService.handleTransactionMethods(reqBody);
-    console.log('response', result);
-    return result;
+
+    res.status(HttpStatus.OK).send(result);
+    return;
   }
 }
