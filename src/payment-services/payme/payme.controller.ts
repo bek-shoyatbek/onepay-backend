ed93078c64d1e6e97737a8fe971983e96e217986
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-20 18:05:05 +0500

    test: payme response logging added

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -19,9 +19,9 @@ export class PaymeController {
   @UseGuards(PaymeBasicAuthGuard)
   @HttpCode(HttpStatus.OK)
   async handleTransactionMethods(@Body() reqBody: RequestBody) {
-
+    console.log("reqBody: ", reqBody);
     const result = await this.paymeService.handleTransactionMethods(reqBody);
-    console.log('response', result);
+    console.log('response: ', result);
     return result;
   }
 }
