da0774e5458e21339c579a42a45346775209a8ed
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-20 17:07:17 +0500

    fix: returning way changed

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -20,11 +20,11 @@ export class PaymeController {
   @Post()
   @UseGuards(PaymeBasicAuthGuard)
   @HttpCode(HttpStatus.OK)
-  async handleTransactionMethods(@Body() reqBody: RequestBody, @Res({ passthrough: true }) res: Response) {
+  async handleTransactionMethods(@Body() reqBody: RequestBody) {
 
     console.log("reqBody: ", reqBody);
     const result = await this.paymeService.handleTransactionMethods(reqBody);
 
-    return res.status(HttpStatus.OK).send(result);
+    return result;
   }
 }
