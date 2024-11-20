8d6ab3ffc356b8d51ed0df65366829219d115aae
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-20 17:12:09 +0500

    fix: uncessary code removed

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -4,13 +4,11 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  Res,
   UseGuards,
 } from '@nestjs/common';
 import { PaymeService } from './payme.service';
 import { RequestBody } from './types/incoming-request-body';
 import { PaymeBasicAuthGuard } from 'src/auth/guards/payme.guard';
-import { Response } from 'express';
 
 @Controller('payme')
 export class PaymeController {
