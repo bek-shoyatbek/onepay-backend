861bc4bb3839798700b24baa9f7fc7118df44488
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-27 17:03:33 +0500

    fix: payme working

--- src/payment-services/payme/payme.controller.ts
+++ src/payment-services/payme/payme.controller.ts
@@ -12,14 +12,13 @@ import { PaymeBasicAuthGuard } from 'src/auth/guards/payme.guard';
 
 @Controller('payme')
 export class PaymeController {
-  constructor(private readonly paymeService: PaymeService) { }
-
+  constructor(private readonly paymeService: PaymeService) {}
 
   @Post()
   @UseGuards(PaymeBasicAuthGuard)
   @HttpCode(HttpStatus.OK)
   async handleTransactionMethods(@Body() reqBody: RequestBody) {
-    console.log("reqBody: ", reqBody);
+    console.log('reqBody: ', reqBody);
     const result = await this.paymeService.handleTransactionMethods(reqBody);
     console.log('response: ', result);
     return result;
